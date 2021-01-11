7b14ed0dfdd08152c5e3d684e19344786ff23599
Larry Gamboa <techmedia.lg@gmail.com>
2021-01-10 21:00:11 -0800

    create initial folders and files

---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+-- Drops the employee_info_db if it already exists --
+DROP DATABASE IF EXISTS burgers_db;
+
+-- Create the database employee_info_db and specified it for use --
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
