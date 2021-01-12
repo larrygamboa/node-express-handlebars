32992220f11ca6235428d6a1a3bbce47d5999094
Larry Gamboa <techmedia.lg@gmail.com>
2021-01-12 15:53:21 -0800

    complete burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,32 @@
 // Import the ORM to create functions that will interact with the database
 const orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function(cb) {
+        // Call back ORM
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+    insertOne: function(cols, vals, cb) {
+        // Call back ORM
+        orm.insertOne("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    updateOne: function(objColVals, condition, cb) {
+        // Call back ORM
+        orm.updateOne("burgers", objColVals, condition, function(res) {
+            cb(res);
+        });
+    },
+    deleteOne: function(condition, cb) {
+        // Call back ORM
+        orm.updateOne("burgers", condition, function(res) {
+            cb(res);
+        });
+    }
+};
+
+// Export the database functions for the controller
+module.exports = burger;
