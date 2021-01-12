2be9824c2288916c2115df63d32bd46212b9059e
Larry Gamboa <techmedia.lg@gmail.com>
2021-01-12 10:41:19 -0800

    set up the server

---
+++ server.js
@@ -0,0 +1,31 @@
+// Required dependencies
+const express = require("express");
+const app = express();
+
+// Define PORT
+const PORT = process.env.PORT || 3000;
+
+// Serve static content for the app
+// from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set handlebars
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+const routes = require("./controllers/controller.js");
+
+app.use(routes);
+
+// Start the server
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
