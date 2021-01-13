e06d2826bee3ab029d3e897a096c2e67ff57b8aa
Larry Gamboa <techmedia.lg@gmail.com>
2021-01-12 16:01:48 -0800

    start controller.js

---
+++ controllers/controller.js
@@ -0,0 +1,10 @@
+// Required dependencies
+const express = require("express");
+const router = express.Router();
+
+// Import the model (burger.js)
+const burger = require("../models/burger.js");
+
+
+// Export routes for server.js to use.
+module.exports = router;
